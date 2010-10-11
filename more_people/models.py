2d4c4e9040b1465bbc5cb38589d193000a457e68
Chip Tol <wiremine@gmail.com>
2010-10-10 21:31:49 -0400

    Removing boilerplate code that isn't needed

--- more_people/models.py
+++ more_people/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-#from django.contrib.contenttypes.models import ContentType
-#from django.contrib.contenttypes import generic
-#from tinymce import models as tinymce_models
-#from filebrowser.fields import FileBrowseField
 
 from people.models import AbstractPerson
 
--- people/models.py
+++ people/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-#from django.contrib.contenttypes.models import ContentType
-#from django.contrib.contenttypes import generic
-#from tinymce import models as tinymce_models
-#from filebrowser.fields import FileBrowseField
 
 from app import get_model_label_by_role
 
